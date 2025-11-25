3641de2fdcd75d411390728e54f76f1d82d9e6aa
yevhenii-sulim <bees127@ukr.net>
2025-11-25 15:45:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <span className='text-white text-xl'>Hello world!</span>
+    </div>
+  );
+}
+
+export default App;
